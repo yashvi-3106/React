892646e0d160a1bfd6ac6ae97f9ae6245e706cab
yashvi-3106 <yashvidholakiya.cg@gmail.com>
2024-12-03 17:35:14 +0530

    done

--- vite_app/src/main.jsx
+++ vite_app/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import Homepage from './Homepage.jsx'
+import './style.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Homepage/>
+    <style/>
   </StrictMode>,
 )
