88ffeb91f18a6618b664142f0fafa6fdd7c70e36
yashvi-3106 <yashvidholakiya.cg@gmail.com>
2024-12-04 10:25:03 +0530

    babel error fixed

--- vite_app/vite.config.js
+++ vite_app/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: './', // Ensure the root is set to your project root
+  publicDir: 'public', // Optional, if you have a separate directory for static files
+  build: {
+    outDir: 'dist' // Directory where the build will be output
+  }
 })
+
+
+
+
